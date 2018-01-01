1cf1ac72a65ec5fc0aac2ae22b484241403341a4
Alexey Halaidzhy <a.halaidzhy@corp.mail.ru>
2018-01-01 20:37:36 +0300

    Занятие 1: Первый проект Hello world

---
+++ lesson1/firstProject/src/main/java/edu/sirius/examples/java/App.java
@@ -0,0 +1,13 @@
+package edu.sirius.examples.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
