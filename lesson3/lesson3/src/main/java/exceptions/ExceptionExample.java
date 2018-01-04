5789349648703947627415c7dea9b5179b82b93c
Alexey Halaidzhy <a.halaidzhy@corp.mail.ru>
2018-01-04 23:57:31 +0300

    Занятие 3: Добавлены более сложные примеры по работе с исключениями

--- lesson3/lesson3/src/main/java/exceptions/ExceptionExample.java
+++ lesson3/lesson3/src/main/java/exceptions/ExceptionExample.java
@@ -1,5 +1,7 @@
 package exceptions;
 
+import java.io.IOException;
+
 public class ExceptionExample {
     private static DatabaseManager getDatabaseManager() {
         return null;
@@ -9,6 +11,7 @@ public class ExceptionExample {
         try {
             getId(name);
         } catch (DBException e) {
+            e.printStackTrace();
             System.err.println(e.getMessage());
         } finally {
             System.out.println("setIdFromName done");
@@ -23,7 +26,42 @@ public class ExceptionExample {
         return manager.readId(name);
     }
 
+    private static int getSize(String str) {
+        try {
+            return str.length();
+        } catch (Exception e) {
+            System.out.println("in catch block");
+            // System.exit(-1); // завершение программы - finally тоже не будет выполнен
+            return -1;
+        } finally {
+            return 0; // перехватит return -1 в случае исключения!
+        }
+    }
+
+    private static void withoutCatch() {
+        try {
+            try {
+                throw new Exception("a");
+            } finally {
+                if (true) {
+                    throw new IOException("b");
+                }
+                System.err.println("c");
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        } catch (Exception e) {
+            System.err.println("d");
+            System.err.println(e.getMessage());
+        }
+    }
+
     public static void run() {
         setIdFromName("test");
+
+        System.out.println(getSize("test"));
+        System.out.println(getSize(null));
+
+        withoutCatch();
     }
 }
