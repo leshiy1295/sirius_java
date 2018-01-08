09ccb257ffc064f1b86721e8e011f57d2c42afa5
Alexey Halaidzhy <a.halaidzhy@corp.mail.ru>
2018-01-09 01:45:02 +0300

    Занятие 5. Добавлено Log4j2 логирование

--- lesson5/lesson5/src/main/java/edu/sirius/examples/java/App.java
+++ lesson5/lesson5/src/main/java/edu/sirius/examples/java/App.java
@@ -1,6 +1,8 @@
 package edu.sirius.examples.java;
 
 import annotations.AnnotationsExample;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 /**
  * Hello world!
@@ -9,6 +11,8 @@ import annotations.AnnotationsExample;
 public class App 
 {
     public static void main( String[] args ) {
+        PropertyConfigurator.configure("log4j.properties");
+        Logger.getLogger("main").debug("main started");
         AnnotationsExample.run();
     }
 }
